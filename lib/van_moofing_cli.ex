f7432548a40493a96a6398070207390b9a270761
g.assies <gertjan.assies@gmail.com>
2020-02-02 11:09:30 +0100

    Prettied up the cli output

--- lib/van_moofing_cli.ex
+++ lib/van_moofing_cli.ex
@@ -5,70 +5,15 @@ defmodule VanMoofing.CLI do
   """
 
   name "VanMoofing"
-  description "Record km cycled on a van Moof"
-
-  command :add do
-    aliases [:a]
-    description "[km] Adds the current total nr of kilometers value"
-
-    argument :km, type: :integer, help: "Current Km's as shown in the Van Moof App"
-
-    run context do
-      VanMoofing.add(get_date(), context[:km])
-    end
-  end
-
-  command :add_after do
-    aliases [:aa]
-    description "[date] [km] Adds the current total nr of kilometers value at the specified date"
-
-    argument :date, type: :string, help: "The particular date a certain amounts of Km are cycles"
-    argument :km, type: :integer, help: "Current Km's as shown in the Van Moof App"
-
-    run context do
-      VanMoofing.add(context[:date], context[:km])
-    end
-  end
-
-  # command :list_all do
-  #   aliases [:la]
-  #   description("List all stored values")
-  #   run _context do
-  #     VanMoofing.list_all()
-  #       |> Enum.each(fn vm -> pr(vm) end)
-  #   end
-  # end
-
-  command :list do
-    aliases [:l]
-    description("[year] List all stored values for the specified year")
-
-    argument :year, type: :integer, help: "[year] The year to list"
-    run context do
-      IO.puts "Date      \tKm's cycled"
-      year = context[:year] |> Integer.to_string
-      VanMoofing.list(year) |> Enum.each(fn vm -> pr(vm) end)
-    end
-  end
-
-  command :trend do
-    aliases [:t]
-
-    argument :year, type: :string, help: "[year] how much have km will be cycled at the end of the year"
-    description "[year] Predicts the neumber of Kilometers cycled in a year"
-    run context do
-      year = context[:year]
-      {avg, days, total, this_year} = VanMoofing.trend_eoy(year)
-      IO.puts "With a average of #{Number.Delimit.number_to_delimited(avg)} km a day and #{days} days till the end of the year, "
-      IO.puts "you will probably cycle : #{this_year} km in #{year} for a grand total of #{total} km"
-    end
-  end
+  description "Record km cycled on a bike that keeps a record of the cycled kilometers"
 
   command :export do
     aliases [:e]
 
     argument :year, type: :string, help: "[year] how much have km will be cycled at the end of the year"
-    description "[year] exports the list of kilometers cycled in a year"
+
+    description "Alias: e\t\targs: [year]\t\tExports the list of kilometers cycled in a year in csv format"
+
     run context do
       year = context[:year]
       IO.puts "\"date\", \"km\", \"trend\""
@@ -77,7 +22,58 @@ defmodule VanMoofing.CLI do
       {_, _, total, _} = VanMoofing.trend_eoy(year)
       IO.puts("\"#{year}-12-31\", , #{total}")
     end
+  end
 
+  command :trend do
+    aliases [:t]
+
+    argument :year, type: :string, help: "[year] how much have km will be cycled at the end of the year"
+
+    description "Alias: t\t\targs: [year]\t\tPredicts the number of kilometers cycled in a year"
+
+    run context do
+      year = context[:year]
+      {avg, days, total, this_year} = VanMoofing.trend_eoy(year)
+      IO.puts "With a average of #{Number.Delimit.number_to_delimited(avg)} km a day and #{days} days till the end of the year, "
+      IO.puts "you will probably cycle : #{this_year} km in #{year} for a grand total of #{total} km"
+    end
+  end
+
+  command :list do
+    aliases [:l]
+
+    description("Alias: l\t\targs: [year]\t\tList all stored values for the specified year")
+
+    argument :year, type: :integer, help: "[year] The year to list"
+
+    run context do
+      IO.puts "Date      \tKm's cycled"
+      year = context[:year] |> Integer.to_string
+      VanMoofing.list(year) |> Enum.each(fn vm -> pr(vm) end)
+    end
+  end
+
+  command :add_after do
+    aliases [:aa]
+    description "Alias: aa\targs: [date] [km]\tAdds the current total nr of kilometers value at the specified date"
+
+    argument :date, type: :string, help: "The particular date a certain amounts of Km are cycled"
+    argument :km, type: :integer, help: "Current Km's as shown in the bike/app"
+
+    run context do
+      VanMoofing.add(context[:date], context[:km])
+    end
+  end
+
+  command :add do
+    aliases [:a]
+    description "Alias: a\t\targs: [km] \t\tAdds the current total nr of kilometers value"
+
+    argument :km, type: :integer, help: "Current Km's as shown in the bike/app"
+
+    run context do
+      VanMoofing.add(get_date(), context[:km])
+    end
   end
 
   defp get_date(), do: Date.utc_today |> Date.to_iso8601
